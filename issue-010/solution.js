8c12b95b933185d771ac58f10e1aa39a65654703
Winnie <winniecherop@outlook.com>
2017-03-08 12:43:16 +0300

    bonus drill

--- issue-010/solution.js
+++ issue-010/solution.js
@@ -1,4 +1,13 @@
-// implement your solution here
+function sumEven(a){
+	return a.filter(function(i){
+		if(i%2==0)
+			return i}).reduce(function(a,b){
+				return(a+b)});}
+		function sumOdd(a){
+			return a.filter(function(i){
+				return i%2}).reduce(function(a,b){
+					return a+b});
+			}
 function evenOdd(a) {
-
+	return sumEven(a) - sumOdd(a);
 }
