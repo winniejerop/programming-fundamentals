25aefafc7a79db5a411ffe37a345eede014ff596
Winnie <winniecherop@outlook.com>
2017-03-20 11:44:42 +0300

    Object Values

--- issue-022/solution.js
+++ issue-022/solution.js
@@ -1,4 +1,7 @@
-// implement your solution here
-function objectValues(a) {
 
+function objectValues(o){                                                                                                                                                  var arr=[];
+     var array_new=Object.keys(o).sort();                                                                                                                                                                                                                                                               
+        for(var k in array_new){ 
+          arr.push(o[array_new[k]]);                                                                                                                                                                    }     return arr;                                                       
 }
+
